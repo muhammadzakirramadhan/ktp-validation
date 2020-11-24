a0b1c1205a85291b9d33d72403c64adc43def5d5
Zakir <coder.zakir@hotmail.com>
2020-11-24 07:55:01 +0700

    Update Result

--- core/application.py
+++ core/application.py
@@ -6,6 +6,7 @@ from PIL import Image
 import requests
 from flask import request
 from werkzeug.utils import secure_filename
+import re
 
 def allowed_image(filename):
 	allow = ["JPEG", "JPG", "PNG"]
@@ -18,6 +19,23 @@ def allowed_image(filename):
 		return True
 	else:
 		return False
+
+def word_to_number_converter(word):
+	word_dict = {
+		"L" : "1",
+		'l' : "1",
+		'O' : "0",
+		'o' : "0",
+		'?' : "7"
+	}
+
+	res = ''
+	for letter in word:
+		if letter in word_dict:
+			res += word_dict[letter]
+		else:
+			res += letter
+	return res
 
 def ocr_ktp():
 	if request.method == "POST":
@@ -34,19 +52,50 @@ def ocr_ktp():
 			gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 			th, threshed = cv2.threshold(gray, 127, 255, cv2.THRESH_TRUNC)
 			result = pytesseract.image_to_string((threshed), lang="ind")
-			valid = False
+			result.replace('\n', ' ')
 
-			if 'NIK' in result:
-				valid = True
-			else:
-				valid = False
+			if 'NIK' in result and 'Nama' in result and 'Lahir' in result and 'Jenis kelamin' in result and 'Alamat' in result:
+				for word in result.split("\n"):
+					if "NIK" in word:
+						word = word.split(':')
+						nik = word_to_number_converter(word[-1].replace(" ", ""))
+						continue
 
-			return {
-				'success':True,
-				'results':{
-					'valid':valid
+					if "Nama" in word:
+						word = word.split(':')
+						nama = word[-1]
+
+					if "Lahir" in word:
+						word = word.split(':')
+						tgl_lahir = re.search("([0-9]{2}\-[0-9]{2}\-[0-9]{4})", word[-1])[0]
+						tmp_lahir = word[-1].replace(tgl_lahir, '')		
+						continue
+
+					if 'Darah' in word:
+						jenis_kelamin = re.search("(LAKI-LAKI|LAKI|LELAKI|PEREMPUAN)", word)[0]
+						word = word.split(':')
+
+				return {
+					'success':True,
+					'message':'valid_ktp',
+					'data': {
+						'nik':nik,
+						'nama':nama,
+						'ttl': tgl_lahir + ', '+ tmp_lahir,
+						'jenis_kelamin':jenis_kelamin
+					}
 				}
-			}
+
+			elif 'NIK' in result:
+				return {
+					'success':True,
+					'message':'blur_ktp'
+				}	
+			else:
+				return {
+					'success':False,
+					'message':'no_valid'
+				}
 		else:	
 			return {
 				'success':False,
